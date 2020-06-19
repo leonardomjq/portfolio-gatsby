eb97dbc347322eb5f8128a7dd618fbc7a99406d0
leonjaques <leonardomjq@gmail.com>
2020-06-19 21:42:49 +0100

    Location of the projects added

--- src/components/Cards/CardRight.js
+++ src/components/Cards/CardRight.js
@@ -8,29 +8,30 @@ export default function CardOne() {
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Before they sold out
-              <br className="hidden lg:inline-block" />
-              readymade gluten
+              Cameron <br className="hidden lg:inline-block" />
+              Wong
             </h1>
             <p className="mb-8 leading-relaxed">
-              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
-              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
-              park mlkshk tote bag selvage hot chicken authentic tumeric
-              truffaut hexagon try-hard chambray.
+              Cameron is a upcoming model and actor raised in Los Angeles.
+              Cameron came asking to build a website that displayed all of his
+              photos and videos in a fashionable way.
             </p>
             <div className="flex justify-center">
-              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                Button
-              </button>
-              <button className="ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded text-lg">
-                Button
-              </button>
+              <p className="inline-flex text-gray-700 border-2 py-2 px-3 focus:outline-none text-md">
+                HTML
+              </p>
+              <p className="ml-3 inline-flex text-gray-700 border-2 py-2 px-3 focus:outline-none text-md">
+                SASS
+              </p>
             </div>
+            <p className="mt-2 leading-relaxed text-sm text-gray-500">
+              Los Angeles, USA.
+            </p>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-6/6">
             <video
               className="object-cover object-center rounded"
-              alt="hero"
+              alt="video of a person scrolling through a webside"
               src={Cameron}
               autoPlay
               loop
